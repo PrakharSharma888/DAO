e4877237539a082778acd97d34040ff702abd403
PrakharSharma888 <prakharentp2002@gmail.com>
2022-05-31 19:35:50 +0530

    Project donee

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -333,6 +333,7 @@ export default function Home() {
           <title>CryptoDevs DAO</title>
           <meta name="description" content="CryptoDevs DAO" />
           <link rel="icon" href="/favicon.ico" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
         </Head>
   
         <div className={styles.main}>
